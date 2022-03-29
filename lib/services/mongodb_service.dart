34e6ad429a46e9bbb25fe02d76b2f8258e47ac42
C19764485 <c19764485@mytudublin.ie>
2022-03-29 12:21:20 +0100

    Fixed app icon

--- lib/services/mongodb_service.dart
+++ lib/services/mongodb_service.dart
@@ -14,12 +14,16 @@ class MongoDB {
 
   http.Client _serverClient = http.Client();
   String baseUri = "https://pedalami.herokuapp.com";
+  // local-host
+  // String baseUri = "http://10.0.2.2:8000";
   static var _dateFormatter = DateFormat("yyyy-MM-ddTHH:mm:ss");
 
   Map<String, String> _headers = {
     'Content-type': 'application/json; charset=utf-8',
     'Accept': 'application/json',
     'Host': 'https://pedalami.herokuapp.com'
+    // local-host
+    // 'Host': '10.0.2.2:8000'
   };
 
   //Use to convert Dart DateTime object to a string whose format matches the one of the backend
